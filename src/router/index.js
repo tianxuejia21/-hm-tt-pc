64f00cbf2447cf5f40d49c22d40d8e017a98429e
e <1750951064@qq.com>
2019-10-26 20:43:58 +0800

    登录与首页与404

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,31 @@ import Vue from 'vue'
 
 import Login from '@/views/login'
 
+import Home from '@/views/home'
+
+import Welcome from '@/views/welcome'
+
+import Notfound from '@/views/404'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [{
     path: '/login',
     component: Login
-  }]
+  },
+  {
+    path: '/',
+    component: Home,
+    children: [{
+      path: '/',
+      component: Welcome
+    }]
+  },
+  {
+    path: '*',
+    component: Notfound
+  }
+  ]
 })
 export default router
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,11 +2,11 @@
   <div class="containner">
     <el-card>
       <img src="../../assets/logo_index.png" width="200px" />
-      <el-form ref="form" :model="loginform">
-        <el-form-item>
-          <el-input v-model="loginform.moblie" placeholder="请输入手机号"></el-input>
+      <el-form ref="loginForm" :model="loginform" :rules="rulesLogin" status-icon>
+        <el-form-item prop="moblie">
+          <el-input v-model="loginform.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="code" status-icon>
           <el-input
             v-model="loginform.code"
             placeholder="请输入验证码"
@@ -18,7 +18,7 @@
           <el-checkbox :value="true">我已阅读并同意协议和隐私条款</el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" style="width:100%">发送验证码</el-button>
+          <el-button type="primary" style="width:100%" @click="login">立即登录</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -28,11 +28,44 @@
 <script>
 export default {
   data () {
+    const checkMoblie = (rule, value, callback) => {
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机号格式不对'))
+      }
+    }
     return {
       loginform: {
-        moblie: '',
+        mobile: '',
         code: ''
+      },
+      rulesLogin: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMoblie, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '验证码6个字符', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs['loginForm'].validate(valid => {
+        if (valid) {
+          this.$http
+            .post('authorizations', this.loginform)
+            .then(res => {
+              this.$router.push('/')
+            })
+            .catch(() => {
+              this.$message.error('手机号或验证码错误')
+            })
+        }
+      })
     }
   }
 }
